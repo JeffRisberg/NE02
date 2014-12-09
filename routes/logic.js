55b4b5fbd36971ffbdae43d66cf6d715ae81f1ba
Jeff Risberg <jrisberg@aol.com>
2014-12-08 16:13:30 -0800

    improved the version numbers, fixed one typo

--- routes/logic.js
+++ routes/logic.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-    var data = {author: {firstName: "Ernest", lastName: "Hemmingway"}};
+    var data = {author: {firstName: "Ernest", lastName: "Hemingway"}};
 
     res.render('logic', data);
 });
