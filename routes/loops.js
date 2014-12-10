9115a325b18717da3309984c519b08a791a58cf0
Jeff Risberg <jrisberg@aol.com>
2014-12-09 16:39:01 -0800

    added more examples

--- routes/loops.js
+++ routes/loops.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-    var activities = ['Hike', "Bike", "Sleep", "Karate"];
+    var activities = ['Walk', 'Hike', "Bike", "Sleep", "Karate"];
     var data = {activities: activities};
 
     res.render('loops', data);
